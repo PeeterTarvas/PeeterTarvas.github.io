c48fd57c502d0fc16314a583b480b32ab2b42331
PeeterTarvas <peetertarvas@gmail.com>
2021-06-01 10:55:01 +0300

    Clean

---
+++ script.js
@@ -0,0 +1,22 @@
+class Main {
+
+
+    constructor(height, length) {
+
+        this.length_bottom = length
+        this.height = height
+    }
+
+    calculateSize() {
+        return this.height * (this.length_bottom / 2);
+    }
+
+    determineIfRectangleIsEqualSided() {
+        var side = Math.sqrt(((this.length_bottom / 2) ** 2)
+            + ((this.height) ** 2));
+        return side === this.length_bottom;
+    }
+
+
+
+}
